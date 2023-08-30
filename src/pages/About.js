bd3a85a228ae2271c048f098972d677419d36199
Gerald-Star <chimagerald3@gmail.com>
2023-08-30 23:02:42 +0200

    create react BrowserRoute, Router and Route path for Home, About and Contact pages

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+
+import Header from '../components/Header'
+
+export default function About() {
+  return (
+    <>
+    
+      <Header />
+      <h2>Home Page</h2>
+    </>
+  );
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+
+import Header from '../components/Header'
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <h2>Home Page</h2>
+
+    
+    </>
+  )
+}
+
